8881ce04690dc34dba0a2b016af09c46c2c510e4
Fabian Bucher <git@fabianbucher.de>
2020-10-15 14:56:33 +0200

    add example service

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,24 @@
+import { Inject, Injectable } from '@angular/core';
+import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+
+const STORAGE_KEY = 'TEST';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  items: any;
+
+  constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
+
+  public getItems(): any {
+    this.items = this.storage.get(STORAGE_KEY) || [];
+    return this.items;
+  }
+
+  public addItem(item: any): void {
+    this.items.push(item);
+    this.storage.set(STORAGE_KEY, this.items);
+  }
+}
