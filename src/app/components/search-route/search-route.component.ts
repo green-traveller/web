b343b194edac77d7b9587c30e512b6e245db32b2
paschild <schild.paulschild@googlemail.com>
2020-10-16 16:11:54 +0200

    search routes: getDate function - padding with zero

--- src/app/components/search-route/search-route.component.ts
+++ src/app/components/search-route/search-route.component.ts
@@ -18,11 +18,27 @@ export class SearchRouteComponent implements OnInit {
 
   ngAfterViewInit(): void {
     let dateStrings: String[] = new Date().toISOString().split('T');
-    this.dateInput.nativeElement.value = dateStrings[0];
+    this.dateInput.nativeElement.value = this.getDateString();
+    console.log(this.getDateString());
     this.timeInput.nativeElement.value = dateStrings[1].substring(0,5);
   }
 
   handleNowButtonClick(): void {
     this.dateInput.nativeElement.value = new Date().toISOString().split('T')[0];
+  }
+
+  getDateString(): String {
+    const date = new Date();
+    const yearString = date.getFullYear();
+    let monthString = date.getMonth().toString();
+    if (monthString.length < 2) {
+      monthString = `0${monthString}`;
+    }
+    let dayString = date.getDate().toString();
+    if (dayString.length < 2) {
+      dayString = `0${dayString}`;
+    }
+
+    return `${yearString}-${monthString}-${dayString}`;
   }
 }
