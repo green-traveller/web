b6796320199b3146136f7b0fd8f324851ecf58f6
paschild <schild.paulschild@googlemail.com>
2020-10-17 21:29:30 +0200

    route-search-results: created component and link in nav-bar

---
+++ src/app/views/route-search-results/route-search-results.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-route-search-results',
+  templateUrl: './route-search-results.component.html',
+  styleUrls: ['./route-search-results.component.css']
+})
+export class RouteSearchResultsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
