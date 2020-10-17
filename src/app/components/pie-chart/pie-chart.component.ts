74a3b63cb06916c6bd41cedca7d80285f694c442
Alexander Schmidt <alexander.m.schmidt@gmail.com>
2020-10-17 22:13:02 +0200

    Pie Chart: added methods and properties for co2 emissions

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartType, ChartOptions } from 'chart.js';
 
+//import { PersonalBalanceComponent } from 'src/app/views/personal-balance/personal-balance.component';
+
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
+  public avgTranspCO2German: number = 100.0; // replace with current value for average co2-emissions per 30 days caused by transport by a German
+  
+  public currentCo2Data: number[] = [20, 7, 15, 10, 13];
+  
+  public currentCo2Sum = this.currentCo2Data.reduce((pv, cv) => pv + cv, 0);  
+
+  public co2PieChartData: number[]  = this.getCo2PieChartData();
+  
+  getCo2PieChartData(): number[] {
+    this.currentCo2Data.push(this.avgTranspCO2German - this.currentCo2Sum);
+    return this.currentCo2Data
+  };
+
   // Pie
   public pieChartOptions: ChartOptions = {
     responsive: true,
   };
-  public pieChartLabels: string[] = ['Car', 'Motorbike', 'Bus', 'Tram','Train'];
-  public pieChartData: number[] = [40, 10, 25 , 5, 20];
+  public pieChartLabels: string[] = ['Car', 'Motorbike', 'Bus', 'Tram', 'Train', 'Rest Budget'];
+  //public pieChartData: number[] = [40, 10, 25 , 5, 20];
   public pieChartType: ChartType = 'pie';
   public pieChartColors =  [
     {
-      backgroundColor: ["#7ae582", "#25a18e", "#9fffcb", "#00a5cf", "#004e64"],
-      borderColor: ["#7ae582", "#25a18e", "#9fffcb", "#00a5cf", "#004e64"],
+      backgroundColor: ["#7ae582", "#25a18e", "#9fffcb", "#00a5cf", "#004e64", "#F0F0F0"],
+      borderColor: '#fff', //["#7ae582", "#25a18e", "#9fffcb", "#00a5cf", "#004e64"],
       pointBackgroundColor: '#fff',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#9ffcb',
@@ -25,7 +40,7 @@ export class PieChartComponent implements OnInit {
     } 
   ];
   
-  public pieChartLegend:boolean = true
+  public pieChartLegend:boolean; // = true;
 
   // events
   public chartClicked(e:any):void {
