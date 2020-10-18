7e6b6f27003d1e7749bfc084cf01573397631399
Fabian Bucher <git@fabianbucher.de>
2020-10-18 13:50:33 +0200

    preferences: add design for custom-vehicles

---
+++ src/app/models/vehicle.ts
@@ -0,0 +1,7 @@
+export interface Vehicle {
+  id: string;
+  name: string;
+  icon: string;
+  co2: number;
+  active: boolean;
+}
--- src/app/views/preferences/custom-vehicles/custom-vehicles.component.ts
+++ src/app/views/preferences/custom-vehicles/custom-vehicles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faCarSide, faMotorcycle, faBicycle, faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { Vehicle } from '../../../models/vehicle';
 
 @Component({
   selector: 'app-custom-vehicles',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomVehiclesComponent implements OnInit {
 
+  icons = {
+    car: faCarSide,
+    motorcycle: faMotorcycle,
+    bicycle: faBicycle,
+    edit: faEdit,
+    delete: faTrashAlt
+  };
+
+  vehicles = VEHICLES;
+  lastDeletedVehicle = undefined;
+
+  bicycle = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  delete(vehicle: Vehicle): void {
+    if (window.confirm(`Deleting vehicle ${vehicle.name}`)) {
+      // TODO
+    }
+  }
+
+  handleBicycleClick(): void {
+    this.bicycle = !this.bicycle;
+  }
 }
+
+const VEHICLES = [
+  {
+    id: 'uuidv4_0',
+    name: 'H-WR 1337',
+    icon: 'car',
+    co2: 251
+  },
+  {
+    id: 'uuidv4_1',
+    name: 'My speedy bike',
+    icon: 'motorcycle',
+    co2: 78
+  }
+];
