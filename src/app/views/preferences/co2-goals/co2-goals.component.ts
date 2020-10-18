f387e5eda72945b5ab89849543d81be19da31496
Fabian Bucher <git@fabianbucher.de>
2020-10-18 15:08:21 +0200

    preferences: add co2 goals design

--- src/app/views/preferences/co2-goals/co2-goals.component.ts
+++ src/app/views/preferences/co2-goals/co2-goals.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-co2-goals',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Co2GoalsComponent implements OnInit {
 
+  // https://www.worldometers.info/co2-emissions/germany-co2-emissions/
+  defaultValues = {
+    average: 148, // (per capita * 0.189) / 12
+    lower: 100
+  };
+
+  model = 'average';
+  customAmount = this.defaultValues.average;
+
+  currentAmount = this.defaultValues.average;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleRadioChange(): void {
+    if (this.model !== 'custom') {
+      this.customAmount = this.defaultValues[this.model];
+      this.currentAmount = this.defaultValues[this.model];
+    }
+  }
+
+  handleCustomAmountChange(): void {
+    // TODO
+  }
 }
