277566bb7515778acb7c4246180691b685b83a27
Fabian Bucher <git@fabianbucher.de>
2020-10-19 11:40:37 +0200

    preferences: sort import in app.modules and app-routing.modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { SearchRouteComponent } from './views/search-route/search-route.component';
+
+import { AboutComponent } from './views/about/about.component';
+import { Co2GoalsComponent } from './views/preferences/co2-goals/co2-goals.component';
+import { CustomVehiclesComponent } from './views/preferences/custom-vehicles/custom-vehicles.component';
+import { EditVehicleComponent } from './views/preferences/custom-vehicles/edit-vehicle/edit-vehicle.component';
+import { ManageDataComponent } from './views/preferences/manage-data/manage-data.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { PreferencesComponent } from './views/preferences/preferences.component';
-import { AboutComponent } from './views/about/about.component';
-import { CustomVehiclesComponent } from './views/preferences/custom-vehicles/custom-vehicles.component';
-import { Co2GoalsComponent } from './views/preferences/co2-goals/co2-goals.component';
-import { ManageDataComponent } from './views/preferences/manage-data/manage-data.component';
-import { EditVehicleComponent } from './views/preferences/custom-vehicles/edit-vehicle/edit-vehicle.component';
+import { SearchRouteComponent } from './views/search-route/search-route.component';
 
 const routes: Routes = [
   { path: '', component: SearchRouteComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'nav', component: NavBarComponent },
   { path: 'preferences', component: PreferencesComponent },
   { path: 'preferences/manage-data', component: ManageDataComponent },
   { path: 'preferences/co2-goals', component: Co2GoalsComponent },
   { path: 'preferences/vehicles', component: CustomVehiclesComponent },
-  { path: 'preferences/vehicles/:id', component: EditVehicleComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'nav', component: NavBarComponent }
+  { path: 'preferences/vehicles/:id', component: EditVehicleComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SearchRouteComponent } from './views/search-route/search-route.component';
+import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { AboutComponent } from './views/about/about.component';
+import { AppComponent } from './app.component';
+import { Co2GoalsComponent } from './views/preferences/co2-goals/co2-goals.component';
+import { CustomVehiclesComponent } from './views/preferences/custom-vehicles/custom-vehicles.component';
+import { EditVehicleComponent } from './views/preferences/custom-vehicles/edit-vehicle/edit-vehicle.component';
+import { ManageDataComponent } from './views/preferences/manage-data/manage-data.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { PreferencesComponent } from './views/preferences/preferences.component';
-import { AboutComponent } from './views/about/about.component';
-import { ManageDataComponent } from './views/preferences/manage-data/manage-data.component';
-import { CustomVehiclesComponent } from './views/preferences/custom-vehicles/custom-vehicles.component';
-import { Co2GoalsComponent } from './views/preferences/co2-goals/co2-goals.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { EditVehicleComponent } from './views/preferences/custom-vehicles/edit-vehicle/edit-vehicle.component';
+import { SearchRouteComponent } from './views/search-route/search-route.component';
 import { VehicleIconComponent } from './components/vehicle-icon/vehicle-icon.component';
 
 @NgModule({
   declarations: [
+    AboutComponent,
     AppComponent,
-    SearchRouteComponent,
+    Co2GoalsComponent,
+    CustomVehiclesComponent,
+    EditVehicleComponent,
+    ManageDataComponent,
     NavBarComponent,
     PreferencesComponent,
-    AboutComponent,
-    ManageDataComponent,
-    CustomVehiclesComponent,
-    Co2GoalsComponent,
-    EditVehicleComponent,
+    SearchRouteComponent,
     VehicleIconComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserModule,
+    FontAwesomeModule,
     FormsModule,
     NgbModule,
-    FontAwesomeModule
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
