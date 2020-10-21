af273e8ba77029b5d6b2943eecb652ffe3053fac
Fabian Bucher <git@fabianbucher.de>
2020-10-21 22:57:02 +0200

    places-api: example for directions

--- src/app/components/places-search/places-search.component.ts
+++ src/app/components/places-search/places-search.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import {} from 'googlemaps';
 import {MapsSdkService} from '../../services/maps-sdk.service';
+import PlaceResult = google.maps.places.PlaceResult;
 
 
 @Component({
@@ -11,7 +12,13 @@ import {MapsSdkService} from '../../services/maps-sdk.service';
 export class PlacesSearchComponent implements OnInit, AfterViewInit {
 
   @Input() labelText: string;
-  autocomplete: google.maps.places.Autocomplete;
+  autocompleteOrigin: google.maps.places.Autocomplete;
+  autocompleteDestination: google.maps.places.Autocomplete;
+  origin: PlaceResult = { name: 'Ernst-Reuter-Platz, Ernst-Reuter-Platz, Berlin, Germany', place_id: 'ChIJreRG9xxRqEcR-Mo9Zk4DQ2Q' };
+  destination: PlaceResult = { name: 'HWR Berlin - Haus 5, Alt-Friedrichsfelde, Berlin, Germany', place_id: 'ChIJD70Jp0pJqEcRe9sSSbbRPp8' };
+  directionsService: google.maps.DirectionsService;
+  routeDistance: string;
+  routeDuration: string;
 
   constructor(private mapsSdkService: MapsSdkService) { }
 
@@ -20,17 +27,45 @@ export class PlacesSearchComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.mapsSdkService.onload(this.initAutocomplete.bind(this));
+    this.mapsSdkService.onload(this.initDirectionsService.bind(this));
   }
 
   initAutocomplete(): void {
     // https://developers.google.com/maps/documentation/javascript/places-autocomplete?hl=en_US#add-autocomplete
-    this.autocomplete = new google.maps.places.Autocomplete(
-      document.getElementById('autocomplete') as HTMLInputElement,
+    this.autocompleteOrigin = new google.maps.places.Autocomplete(
+      document.getElementById('autocompleteOrigin') as HTMLInputElement,
       { componentRestrictions: { country: ['DE'] } }
     );
     // https://developers.google.com/maps/documentation/javascript/place-data-fields?hl=en_US
-    this.autocomplete.setFields(['place_id', 'name']);
+    this.autocompleteOrigin.setFields(['place_id', 'name']);
     // When the user selects an address from the drop-down
-    this.autocomplete.addListener('place_changed', () => console.log(this.autocomplete.getPlace()));
+    this.autocompleteOrigin.addListener('place_changed', () => this.origin = this.autocompleteOrigin.getPlace());
+
+    // dest
+    this.autocompleteDestination = new google.maps.places.Autocomplete(
+      document.getElementById('autocompleteDestination') as HTMLInputElement,
+      { componentRestrictions: { country: ['DE'] } }
+    );
+    this.autocompleteDestination.setFields(['place_id', 'name']);
+    this.autocompleteDestination.addListener('place_changed', () => this.destination = this.autocompleteDestination.getPlace());
+  }
+
+  initDirectionsService(): void {
+    this.directionsService = new google.maps.DirectionsService();
+  }
+
+  calcRoute(): void {
+    const request = {
+      origin: { placeId: this.origin.place_id },
+      destination: { placeId: this.destination.place_id },
+      travelMode: 'DRIVING'
+    };
+    console.log('Calculating...');
+    // @ts-ignore
+    this.directionsService.route(request, (result, status) => {
+      this.routeDistance = result.routes[0].legs[0].distance.text;
+      this.routeDuration = result.routes[0].legs[0].duration.text;
+      console.log(result, result.routes[0].legs[0].distance, this.routeDistance);
+    });
   }
 }
