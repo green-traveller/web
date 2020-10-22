4024a3e8548d970e5d42efa7b94d751d0eed87c0
paschild <schild.paulschild@googlemail.com>
2020-10-22 18:55:11 +0200

    route-search-results: accordion-panel as ngFor-loop

--- src/app/models/route.ts
+++ src/app/models/route.ts
@@ -2,6 +2,7 @@ import { RouteLocation } from './route-location';
 import { RouteOption } from './route-option';
 
 export interface Route {
+  id: string;
   from: RouteLocation;
   to: RouteLocation;
   time: string;
--- src/app/views/route-search-results/route-search-results.component.ts
+++ src/app/views/route-search-results/route-search-results.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Route } from 'src/app/models/route';
+import { Vehicle } from 'src/app/models/vehicle';
+import { DataService } from 'src/app/services/data.service';
 import {
   faCarSide,
   faMotorcycle,
@@ -21,6 +25,45 @@ export class RouteSearchResultsComponent implements OnInit {
       train: faSubway,
       walking: faWalking
     };
+
+    public routeResults: Route[] = [
+      {
+        id: 'a',
+        from: {name: 'start', place_id: '1'},
+        to: {name: 'destination', place_id: '2'},
+        time: '01-01-1970',
+        vehicleId: 'walking',
+        passengers: 1,
+        options: { walking: { distance: 30, duration: 40}}
+      },
+      {
+        id: 'b',
+        from: {name: 'start', place_id: '1'},
+        to: {name: 'destination', place_id: '2'},
+        time: '01-01-1970',
+        vehicleId: 'bicycling',
+        passengers: 1,
+        options: { bicycling: { distance: 30, duration: 40}}
+      },
+      {
+        id: 'c',
+        from: {name: 'start', place_id: '1'},
+        to: {name: 'destination', place_id: '2'},
+        time: '01-01-1970',
+        vehicleId: 'transit',
+        passengers: 1,
+        options: { transit: { distance: 30, duration: 40}}
+      },
+      {
+        id: 'd',
+        from: {name: 'start', place_id: '1'},
+        to: {name: 'destination', place_id: '2'},
+        time: '01-01-1970',
+        vehicleId: 'driving',
+        passengers: 1,
+        options: { driving: { distance: 30, duration: 40}}
+      },
+    ];
 
   constructor() { }
 
