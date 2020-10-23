f2cd9c265ac2b9d22cef091c08b8084d0a975fb6
Spikeone <fvv1@gmx.de>
2020-10-23 13:04:29 +0200

    add icon service

---
+++ src/app/services/icon.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import {
+  faCarSide,
+  faMotorcycle,
+  faBicycle,
+  faWalking,
+  faSubway,
+  faQuestion,
+  faEdit,
+  faTrashAlt,
+  faMapMarked,
+  faClock,
+  faUsers,
+  faRoute,
+  IconDefinition
+} from '@fortawesome/free-solid-svg-icons';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IconService {
+
+  APP_ICONS = {
+    unknown: faQuestion,
+    car: faCarSide,
+    motorcycle: faMotorcycle,
+    bicycle: faBicycle,
+    train: faSubway,
+    walking: faWalking,
+    edit: faEdit,
+    delete: faTrashAlt,
+    map: faMapMarked,
+    clock: faClock,
+    users: faUsers,
+    distance: faRoute
+  };
+
+  getIcon(name: string): IconDefinition{
+    if (this.APP_ICONS[name]) {
+      return this.APP_ICONS[name];
+    } else {
+      return this.APP_ICONS.unknown;
+    }
+  }
+
+  constructor() { }
+}
