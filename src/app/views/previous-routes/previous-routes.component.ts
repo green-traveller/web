c42493cf8024628767d099484fec4b412017e4c4
Spikeone <fvv1@gmx.de>
2020-10-24 14:23:12 +0200

    cleanup code, use services for data transformation, functional previous route dialogue

--- src/app/views/previous-routes/previous-routes.component.ts
+++ src/app/views/previous-routes/previous-routes.component.ts
@@ -5,6 +5,7 @@ import { APP_ICONS } from 'src/app/models/app-icons';
 import { Vehicle } from 'src/app/models/vehicle';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IconService } from 'src/app/services/icon.service';
+import { RouteService } from 'src/app/services/route.service';
 
 @Component({
   selector: 'app-previous-routes',
@@ -13,27 +14,13 @@ import { IconService } from 'src/app/services/icon.service';
 })
 export class PreviousRoutesComponent implements OnInit {
 
-  public prevRoutes: Route[] = [
-    {id: 'a', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'walking', passengers: 1, options: { walking: { distance: 30, duration: 40}, driving: {distance: 20, duration: 10}}},
-    {id: 'a', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'bicycling', passengers: 1, options: { bicycling: { distance: 30, duration: 40}}},
-    {id: 'a', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'transit', passengers: 1, options: { transit: { distance: 30, duration: 40}}},
-    {id: 'a', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'driving', passengers: 1, options: { driving: { distance: 30, duration: 40}}},
-    {id: 'a', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'walking', passengers: 4, options: { walking: { distance: 30, duration: 40}}},
-    {id: 'b', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'bicycling', passengers: 4, options: { bicycling: { distance: 30, duration: 40}}},
-    {id: 'b', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'transit', passengers: 4, options: { transit: { distance: 30, duration: 40}}},
-    {id: 'b', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'driving', passengers: 4, options: { driving: { distance: 30, duration: 40}}},
-    {id: 'b', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'thisIsInvalid', passengers: 4, options: { walking: { distance: 30, duration: 40}}},
-    {id: 'b', from: {name: 'start', place_id: '1'}, to: {name: 'destination', place_id: '2'}, time: '01-01-1970', vehicleId: 'driving', passengers: 4, options: { }}
-  ];
-
-  public vehicle;
-
-  // TODO: How to use the correct type?
-  public icons;
-
   private modalRoute: Route;
 
-  constructor(private dataService: DataService, private iconService: IconService, private modalService: NgbModal) { }
+  constructor(
+    private dataService: DataService,
+    private iconService: IconService,
+    private routeService: RouteService,
+    private modalService: NgbModal) { }
 
   open(content: any, route: Route): any {
     this.modalRoute = route;
@@ -42,68 +29,14 @@ export class PreviousRoutesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.icons = APP_ICONS;
-    this.vehicle = this.dataService.getVehicles();
   }
 
-  // TODO: Move to a route service
-  getRouteCO2(route: Route): number {
-    let routeCO2 = 0;
-    const vehicle = this.dataService.getVehicle(route.vehicleId);
-
-    if (vehicle && route.options[route.vehicleId]) {
-      // Guess CO2 is always per 100km
-      routeCO2 = vehicle.co2 * route.options[route.vehicleId].distance / 1000;
-
-      // using transit with multiple passengers does not reduce CO2 per person
-      if (route.vehicleId !== 'transit') {
-        routeCO2 /= route.passengers;
-      }
-    }
-
-    return routeCO2;
+  getRoutes() {
+    return Object.values(this.dataService.getRoutes());
   }
 
-  getVehicleType(route: Route): string {
-    const vehicle = this.dataService.getVehicle(route.vehicleId);
-
-    if (vehicle) {
-      return vehicle.type;
-    } else {
-      return 'unknown';
-    }
-  }
-
-  editVehicleForRoute(route: Route): void {
-    console.log('editVehicleForRoute');
-    console.log(route);
-  }
-
-  // TODO: Move to a route service (and data-service)
   deleteRoute(route: Route): void {
-    console.log('deleteRoute');
-    console.log(route);
-  }
-
-  // TODO: Implement
-  // TODO: Move to a route service
-  hasRouteMap(route: Route): boolean {
-    return route.id === 'b';
-  }
-
-  // TODO: Move to a route Service
-  getRouteMapLink(route: Route): string {
-    let mapLink = 'https://www.google.de/maps/preview';
-
-    return mapLink;
-  }
-
-  getRouteDuration(route: Route): number {
-    return route.options[route.vehicleId].duration;
-  }
-
-  getRouteDistance(route: Route): number {
-    return route.options[route.vehicleId].distance;
+    this.dataService.deleteRoute(route);
   }
 
   validatePassengerInput(event: any, route: Route): void {
@@ -113,26 +46,15 @@ export class PreviousRoutesComponent implements OnInit {
 
     route.passengers = event.target.value;
 
-    // TODO: actually save this
+    this.dataService.setRoutePassengers(route, event.target.value);
   }
 
   getActiveVehicles(): Vehicle[] {
     return this.dataService.getActiveVehicles();
   }
 
-  getDefaultVehicles(): Vehicle[] {
-    return this.dataService.getDefaultVehicles();
-  }
-
-  getCustomVehicles(): Vehicle[] {
-    return this.dataService.getCustomActiveVehicles();
-  }
-
   setVehicleType(vehicle: Vehicle): void {
-    console.log('set vehicle');
-    console.log(this.modalRoute.id);
-    console.log(vehicle.id);
+    this.dataService.setRouteVehicle(this.modalRoute, vehicle.id);
     this.modalService.dismissAll();
   }
-
 }
