3c88b17927dd4d9b9046bcebf9a5c8f99114d06e
Fabian Bucher <git@fabianbucher.de>
2020-10-24 14:07:24 +0200

    meta: set-env - change strategy

--- set-env.js
+++ set-env.js
@@ -1,16 +1,17 @@
 // https://medium.com/@ferie/how-to-pass-environment-variables-at-building-time-in-an-angular-application-using-env-files-4ae1a80383c
 const fs = require('fs');
 // Configure Angular `environment.ts` file path
-const targetPath = './src/environments/env.ts';
+const targetPath = './src/environments/environment.prod.ts';
 // `environment.ts` file structure
-const envConfigFile = `export const env = {
-   apiKey: '${process.env.MAPS_API_KEY}'
+const envConfigFile = `export const environment = {
+    production: true,
+    apiKey: '${process.env.MAPS_API_KEY}'
 };
 `;
 fs.writeFile(targetPath, envConfigFile, (err) => {
   if (err) {
     throw console.error(err);
   } else {
-    console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+    console.log(`Angular environment.prod.ts file generated correctly at ${targetPath} \n`);
   }
 });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,3 @@
-// @ts-ignore
-import { env } from './env.ts';
-
 export const environment = {
-  production: true,
-  apiKey: env.apiKey
+  production: true
 };
