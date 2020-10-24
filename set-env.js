494a34425844fc5436091f008665273f7f6a5a34
Fabian Bucher <git@fabianbucher.de>
2020-10-24 13:53:57 +0200

    meta: set-env fix errors

--- set-env.js
+++ set-env.js
@@ -1,5 +1,5 @@
 // https://medium.com/@ferie/how-to-pass-environment-variables-at-building-time-in-an-angular-application-using-env-files-4ae1a80383c
-import { writeFile } from 'fs';
+const fs = require('fs');
 // Configure Angular `environment.ts` file path
 const targetPath = './src/environments/environment.ts';
 // `environment.ts` file structure
@@ -7,9 +7,7 @@ const envConfigFile = `export const environment = {
    apiKey: '${process.env.MAPS_API_KEY}'
 };
 `;
-console.log('The file `environment.ts` will be written with the following content: \n');
-console.log(envConfigFile);
-writeFile(targetPath, envConfigFile, (err) => {
+fs.writeFile(targetPath, envConfigFile, (err) => {
   if (err) {
     throw console.error(err);
   } else {
