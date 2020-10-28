8337bcbdbaba144023e7732cc2129daa9e079d98
Fabian Bucher <git@fabianbucher.de>
2020-10-28 23:49:14 +0100

    installation-instruction: use component

--- src/app/components/installation-instructions/installation-instructions.component.ts
+++ src/app/components/installation-instructions/installation-instructions.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+const STEPS = {
+  android: [ { image: 'share_android' }, { image: 'add_android', small: true }],
+  iOS: [ { image: 'share_apple' }, { image: 'add_apple' } ],
+  samsung: [ { image: 'add_samsung'} ]
+};
+
 @Component({
   selector: 'app-installation-instructions',
   templateUrl: './installation-instructions.component.html',
@@ -8,23 +14,29 @@ import { Component, OnInit } from '@angular/core';
 export class InstallationInstructionsComponent implements OnInit {
 
   os: string;
+  steps: any;
 
   constructor() { }
 
   ngOnInit(): void {
     this.os = this.getMobileOperatingSystem();
+    if (this.os === 'unknown') {
+      this.steps = STEPS.samsung;
+    } else {
+      this.steps = STEPS[this.os];
+    }
   }
 
   getMobileOperatingSystem(): string {
     const userAgent = (navigator.userAgent || navigator.vendor);
     if (/android/i.test(userAgent)) {
-      return 'Android';
+      return 'android';
     }
     if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
       return 'iOS';
     }
     if (/SAMSUNG|SGH-[I|N|T]|GT-[I|P|N]|SM-[N|P|T|Z|G]|SHV-E|SCH-[I|J|R|S]|SPH-L/i.test(userAgent)) {
-      return 'Samsung';
+      return 'samsung';
     }
     return 'unknown';
   }
