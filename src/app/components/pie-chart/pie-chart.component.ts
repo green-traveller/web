0af991d518b8a2e698baa7c84c25d6d49ab58887
Spikeone <fvv1@gmx.de>
2020-10-28 13:28:24 +0100

    improve pie chart tooltips

--- src/app/components/pie-chart/pie-chart.component.ts
+++ src/app/components/pie-chart/pie-chart.component.ts
@@ -1,3 +1,4 @@
+import { formatNumber } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { ChartType, ChartOptions } from 'chart.js';
 
@@ -7,28 +8,46 @@ import { ChartType, ChartOptions } from 'chart.js';
   styleUrls: ['./pie-chart.component.css']
 })
 export class PieChartComponent implements OnInit {
-  
+
   pieChartType: ChartType = 'pie';
-  
-  pieChartOptions: ChartOptions = {    
+
+  pieChartOptions: ChartOptions = {
     responsive: true,
-    legend: {
-        display: true,
-        position: 'bottom'
+    rotation: 30,
+    legend: { position: 'bottom' },
+
+    tooltips: {
+      enabled: true,
+      callbacks: {
+       label(tooltipItem, data): string {
+        const label = data.labels[tooltipItem.index];
+        const value = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].valueOf().toString();
+        return `${label} ${formatNumber(Number(value), 'en_US', '1.2-2')} kg`;
+       },
       },
-    maintainAspectRatio: false       
-  }
+     },
+
+    plugins: {
+     datalabels: {
+      formatter: (value, ctx) => {
+       const label = '0.00';
+       return label;
+      },
+     },
+    },
+    maintainAspectRatio: false
+   };
 
   @Input() pieChartLabels: string[];
 
-  @Input() pieChartData: number[];  
+  @Input() pieChartData: number[];
 
-  @Input() pieChartColors: Array<any>;  
-  
+  @Input() pieChartColors: Array<any>;
+
   @Input() pieChartLegend: boolean;
 
   // events
-  public chartClicked(e:any): void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
 
