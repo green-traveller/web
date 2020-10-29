140ac012a2f47635677b90c4dd29021bc8de4c9e
Alexander Schmidt <alexander.m.schmidt@gmail.com>
2020-10-29 20:19:42 +0100

    Spelling correction CO₂- xy, no dash

--- src/app/views/personal-balance/personal-balance.component.ts
+++ src/app/views/personal-balance/personal-balance.component.ts
@@ -45,7 +45,7 @@ export class PersonalBalanceComponent implements OnInit {
   getPersonalGoalAlertMessage(): string {
     if (this.personalGoalBarStatus < 1) {
       return `Great! In the last 30 days you have been true to your personal goal 
-      and kept your CO₂-emissions from transport ${formatNumber((1 - this.personalGoalBarStatus)*100, 'en_US', '1.1-1')} % lower than ${formatNumber(this.personalGoal, 'en_US', '1.0-0')} kg!`
+      and kept your CO₂ emissions from transport ${formatNumber((1 - this.personalGoalBarStatus)*100, 'en_US', '1.1-1')} % lower than ${formatNumber(this.personalGoal, 'en_US', '1.0-0')} kg!`
     } 
     else if (this.personalGoalBarStatus  === 1) {
       return `Great! You have reached your goal and emitted exactly ${formatNumber(this.currentCo2Sum, 'en_US', '1.0-0')} 
