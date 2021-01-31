02e5cdcbf73d70acc85de25b5bcce9e5302741cb
Alexander Schmidt <alexander.m.schmidt@gmail.com>
2021-01-31 13:10:07 +0100

    Add selector+getter co2PieChart  @PersonalBalance
    Add SocialMediaComponent to Personal Balance + Input+Output
    
    Fix tslint issues

--- src/app/views/personal-balance/personal-balance.component.ts
+++ src/app/views/personal-balance/personal-balance.component.ts
@@ -1,6 +1,7 @@
 import { formatNumber } from '@angular/common';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import Chart from 'chart.js';
+import { Co2PieChartComponent } from 'src/app/components/co2-pie-chart/co2-pie-chart.component';
 import { Alert } from 'src/app/models/alert';
 import { DataService } from 'src/app/services/data.service';
 import { RouteService } from 'src/app/services/route.service';
@@ -14,7 +15,7 @@ export class PersonalBalanceComponent implements OnInit {
 
   currentCo2Sum = this.dataService.getTotalCo2Last30Days(this.routeService);
 
-  @ViewChild('co2Pie') Co2Pie: ElementRef;
+  @ViewChild('co2PieChart') co2PieChart: Co2PieChartComponent;
   // Personal Goal
 
   personalGoal: number = this.dataService.getCo2PersonalChallenge().value;
@@ -28,60 +29,69 @@ export class PersonalBalanceComponent implements OnInit {
 
   showPersonalChallengeAlert = true;
 
+  co2PieChartCanvas: HTMLCanvasElement;
+
   constructor(private dataService: DataService, private routeService: RouteService) { }
 
   ngOnInit(): void {
   }
 
-  ngAfterViewInit(): void {
-    // @ts-ignore
-    const chart = this.Co2Pie.Co2PieChart.PieChart.nativeElement as HTMLCanvasElement;
-    const image = document.createElement('img');
-    image.src = chart.toDataURL('image/png');
+  getCo2PieChart(): Co2PieChartComponent {
+    return this.co2PieChart;
+  }
 
-    const nchart = new Chart(chart, {
-      type: 'pie',
-      // @ts-ignore
-      data: this.Co2Pie.Co2PieChart.PieChart.pieChartData,
-      options: {
-        animation: {
-          onComplete: function() {
-            const exportElement = document.createElement('a');
-            exportElement.setAttribute('href', this.toBase64Image());
-            const exportName = 'image_exp.png';
-            exportElement.setAttribute('download', exportName);
-            exportElement.click();
-            exportElement.remove();
-          }
-        }
-      }
-    });
+  getCo2PieChartCanvas(): HTMLCanvasElement {
+    return this.getCo2PieChart().getPieChart().getPieChartCanvas();
   }
 
   getPersonalGoalAlertType(): string {
     if (this.personalGoalBarStatus < 1) {
-      return 'success'
+      return 'success';
     } else if (this.personalGoalBarStatus === 1) {
-      return 'warning'
-    } 
+      return 'warning';
+    }
     else {
-      return 'danger'
+      return 'danger';
     }
   }
 
   getPersonalGoalAlertMessage(): string {
     if (this.personalGoalBarStatus < 1) {
       return `Great! Over the last 30 days you have mastered your personal challenge and even kept your CO₂ emissions
-      from transport ${formatNumber((1 - this.personalGoalBarStatus)*100, 'en_US', '1.1-1')} % lower!`
+      from transport ${formatNumber((1 - this.personalGoalBarStatus) * 100, 'en_US', '1.1-1')} % lower!`;
     }
     else if (this.personalGoalBarStatus  === 1) {
       return `Great! Over the last 30 days you have mastered your personal challenge and emitted exactly ${formatNumber(this.currentCo2Sum, 'en_US', '1.0-0')}
-      kg of CO₂ from transport. Can you do even better?`
+      kg of CO₂ from transport. Can you do even better?`;
     }
     else {
-      return `Oh no, you failed your personal challenge! Over the last 30 days you have emitted ${formatNumber((this.personalGoalBarStatus - 1)*100, 'en_US', '1.1-1')} % more CO₂
-      from transport than you planned to!`
+      return `Oh no, you failed your personal challenge! Over the last 30 days you have emitted ${formatNumber((this.personalGoalBarStatus - 1) * 100, 'en_US', '1.1-1')} % more CO₂
+      from transport than you planned to!`;
     }
+  }
+
+  triggerImageExport(trigger: boolean): void {
+    if (trigger) {
+      console.log(this.getCo2PieChart().getPieChart().getPieChartCanvas().toDataURL('image/png'));
+    }
+    // const file = new File([this.b64toBlob(this.drawCanvas())], 'image.png', { type: 'image/png'});
+    /*const file = new File(
+      [this.b64toBlob('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8z8BQDwAEhQGAhKmMIQAAAABJRU5ErkJggg==')],
+      'image.gif',
+      { type: 'image/gif' }
+      );*/
+    // console.log(file);
+    // // @ts-ignore
+    // if (navigator.canShare) {
+    //   navigator.share({
+    //     // @ts-ignore
+    //     files: [file],
+    //     title: 'Test',
+    //     url: 'www.google.de'
+    //   });
+    // } else {
+    //   this.export();
+    // }
   }
 
 }
